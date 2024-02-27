c175cff73e774c47bfe7f529b91621cf5699c1a2
Mohd Sameer Ahmad <mohdsameer7408@gmail.com>
2024-02-27 19:13:45 +0530

    added metadata

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -7,6 +7,11 @@ import { CreateInvoice } from '@/app/ui/invoices/buttons';
 import { lusitana } from '@/app/ui/fonts';
 import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
 import { fetchInvoicesPages } from '@/app/lib/data';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Invoices',
+};
 
 const Page = async ({
   searchParams,
--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,5 +1,10 @@
+import { Metadata } from 'next';
 import AcmeLogo from '@/app/ui/acme-logo';
 import LoginForm from '@/app/ui/login-form';
+
+export const metadata: Metadata = {
+  title: 'Login',
+};
 
 const LoginPage = () => {
   return (
